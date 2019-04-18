8fdae6848a2abf6cc75c7ca388ee9c260f74927d
ningyq <1554115736@qq.com>
2019-04-18 12:10:04 +0800

    add Response

---
+++ src/main/java/com/xuptdata/bdll/controller/ClassifyController.java
@@ -0,0 +1,8 @@
+package com.xuptdata.bdll.controller;
+
+/**
+ * @Author: ningyq
+ * @Date: 2019/4/18 11:52
+ */
+public class ClassifyController {
+}
---
+++ src/main/java/com/xuptdata/bdll/entity/Response.java
@@ -0,0 +1,14 @@
+package com.xuptdata.bdll.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: ningyq
+ * @Date: 2019/4/18 12:06
+ */
+@Data
+public class Response {
+    private String status;
+
+    private String msg;
+}
